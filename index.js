2f4109ee3f64d6afd280ee3229eb3818db2efe26
Neil Kistner <neil.kistner@gmail.com>
2016-03-03 20:56:58 -0600

    Add recommended config

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ module.exports = {
   rules: {
     "space-around-conditional": require('./lib/rules/space-around-conditional'),
     "sort-imports": require('./lib/rules/sort-imports')
+  },
+  configs: {
+    recommended: {
+      rules: {
+        'wyze/sort-imports': 2,
+        'wyze/space-around-conditional': 2
+      }
+    }
   }
 }
