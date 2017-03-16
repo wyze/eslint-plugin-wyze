f34279b5e47a9c86b3a9b385e5bf93195f0400fc
Neil Kistner <neil.kistner@gmail.com>
2017-03-15 20:26:11 -0500

    Upgrade lint rules and fix lint errors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+/* eslint-disable global-require */
 module.exports = {
   rules: {
     'max-file-length': require('./lib/rules/max-file-length'),
     'newline-after-export': require('./lib/rules/newline-after-export'),
     'sort-destructuring-keys': require('./lib/rules/sort-destructuring-keys'),
     'sort-imports': require('./lib/rules/sort-imports'),
-    'space-around-conditional': require('./lib/rules/space-around-conditional')
+    'space-around-conditional': require('./lib/rules/space-around-conditional'),
   },
   configs: {
     recommended: {
@@ -13,8 +14,8 @@ module.exports = {
         'wyze/newline-after-export': 'error',
         'wyze/sort-destructuring-keys': 'error',
         'wyze/sort-imports': 'error',
-        'wyze/space-around-conditional': 'error'
-      }
-    }
-  }
+        'wyze/space-around-conditional': 'error',
+      },
+    },
+  },
 }
